4520510cb0037096efaab294d335e85409d36415
trueTriangle1337 <truetriangle1337@moveax.pro>
2016-12-21 15:28:47 +0500

    Add files via upload

---
+++ KeyCode/WindowsFormsApplication13/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label2.Location = new System.Drawing.Point(113, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "#????";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label3.Location = new System.Drawing.Point(0, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label4.Location = new System.Drawing.Point(0, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Code:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label5.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label5.Location = new System.Drawing.Point(4, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Нажмите кнопку и получите её код";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label6.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label6.Location = new System.Drawing.Point(4, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Работает на большие буквы (Shift + KEY)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.PaleGreen;
+            this.label7.Location = new System.Drawing.Point(194, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "GH";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.label7.MouseLeave += new System.EventHandler(this.label7_MouseLeave);
+            this.label7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label7_MouseMove);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.PaleGreen;
+            this.label8.Location = new System.Drawing.Point(151, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Site";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            this.label8.MouseLeave += new System.EventHandler(this.label8_MouseLeave);
+            this.label8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label8_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(227, 155);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "KeyCode";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ KeyCode/WindowsFormsApplication13/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int n;
+            label1.Text = e.KeyChar.ToString();
+            char c;
+            c = Convert.ToChar(label1.Text);
+            n = c;
+            label2.Text = "#" + n.ToString();
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/pTriangle");
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://moveax.pro");
+        }
+
+        private void label8_MouseMove(object sender, MouseEventArgs e)
+        {
+            label8.Text = "CLICK";
+        }
+
+        private void label7_MouseMove(object sender, MouseEventArgs e)
+        {
+            label7.Text = "CLICK";
+        }
+
+        private void label8_MouseLeave(object sender, EventArgs e)
+        {
+            label8.Text = "Site";
+        }
+
+        private void label7_MouseLeave(object sender, EventArgs e)
+        {
+            label7.Text = "GH";
+        }
+    }
+}
